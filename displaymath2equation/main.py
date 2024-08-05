79032288f5048ae11ed9c9f47216b288bfef0167
Fynn Freyer <fynn.freyer@googlemail.com>
2024-08-05 03:28:33 +0200

    feat(config): add config class for option management

--- displaymath2equation/main.py
+++ displaymath2equation/main.py
@@ -1,10 +1,10 @@
 """
 Number all equations in a document, when converting to latex.
 """
-
-from re import compile
-
 from panflute import run_filter, Element, Doc, Math, RawBlock, RawInline, Para, Str
+
+from displaymath2equation.config import Config
+from displaymath2equation.types_ import Walker
 
 
 def is_block_para(elem: Element) -> bool:
@@ -19,38 +19,49 @@ def is_block_para(elem: Element) -> bool:
     return False
 
 
-EQ_REF = compile(r"\(eq:(?P<eq_id>[^)]+)\)")
-
-
-def replace_math(elem: Math) -> RawBlock | None:
+def replace_math(elem: Math, config: Config) -> RawBlock | None:
     """
     If this math element is a display math block, we replace it with the equation environment.
 
     :param elem: A math block.
     :return: A raw latex block with an equation environment, or ``None`` if elem is no display math block.
     """
-    eq_start = r"\begin{equation}"
-    eq_stop = r"\end{equation}"
 
     if elem.format == "DisplayMath":
-        eq_text = f"{eq_start}{elem.text}{eq_stop}"
+        # bail if labeled-only is set in config,
+        # but text doesn't contain \label directive
+        if config.labeled_only and r"\label" not in elem.text:
+            return
+
+        eq_text = f"{config.env_start}{elem.text}{config.env_stop}"
         eq = RawBlock(eq_text, format="latex")
         return eq
 
 
-def replace_eq_refs(elem: Element, doc: Doc | None = None) -> RawInline | None:
+def replace_eq_refs(config: Config) -> Walker:
+    """
+    Generate a walker that replaces equation references.
+
+    :param config: The config object.
+    :return: A walker function.
     """
 
-    :param elem:
-    :param doc:
-    :return:
-    """
-    if isinstance(elem, Str):
-        match = EQ_REF.match(elem.text)
-        if match:
-            eq_id = match['eq_id']
-            eq_ref = RawInline(rf"\eqref{{{eq_id}}}", format="latex")
-            return eq_ref
+    def replacer(elem: Element, doc: Doc | None = None) -> RawInline | None:
+        """
+        Function to walk the element tree and replace occurrences of the equation reference pattern.
+
+        :param elem: An element in the stream.
+        :param doc: Possibly the document we're working on.
+        :return: A changed element or nothing.
+        """
+        if isinstance(elem, Str):
+            match = config.eq_ref_pattern.match(elem.text)
+            if match:
+                eq_id = match['eq_id']
+                eq_ref = RawInline(rf"\eqref{{{eq_id}}}", format="latex")
+                return eq_ref
+
+    return replacer
 
 
 def action(elem: Element, doc: Doc | None = None) -> Element | None:
@@ -60,14 +71,15 @@ def action(elem: Element, doc: Doc | None = None) -> Element | None:
     :param doc: Possibly the document we're working on.
     :return: A changed element or nothing.
     """
+    config = Config.from_doc(doc)
     if is_block_para(elem):
         child = elem.content[0]
         if isinstance(child, Math):
-            return replace_math(child)
+            return replace_math(child, config=config)
 
     # replace eq_refs in paragraphs
     if isinstance(elem, Para):
-        elem.walk(replace_eq_refs)
+        elem.walk(replace_eq_refs(config))
 
 
 def main(doc=None, input_stream=None, output_stream=None):
